24ada4ffaaafb3d7cf47b3a2ed8546b93be79308
mrHoft <ru.hoft@gmail.com>
2023-12-16 00:25:51 +0300

    feat: repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,42 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const add =
+    options.addition === undefined
+      ? ""
+      : new Array(options.additionRepeatTimes)
+          .fill(options.addition || `${options.addition}`)
+          .join(options.additionSeparator || "|");
+  console.log(add);
+  return new Array(options.repeatTimes)
+    .fill(str + add)
+    .join(options.separator || "+");
 }
-
+/* 
+console.log(
+  repeater(null, {
+    repeatTimes: 3,
+    separator: "??? ",
+    addition: null,
+    additionRepeatTimes: 3,
+    additionSeparator: "!!!",
+  }),
+  "nullnull!!!null!!!null??? nullnull!!!null!!!null??? nullnull!!!null!!!null"
+);
+ */
 module.exports = {
-  repeater
+  repeater,
 };
