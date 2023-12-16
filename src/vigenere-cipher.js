1c7be52253796f09619cd43355011672831fd1d4
mrHoft <ru.hoft@gmail.com>
2023-12-16 08:04:36 +0300

    feat: Vigenere, Season, ...rest

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,92 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  alph = "";
+  code = [];
+  direct = true;
+  constructor(direct = true) {
+    this.alph = this.abc + this.abc;
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  gencode(key) {
+    this.code = key.split("").map((c) => this.abc.indexOf(c));
+  }
+
+  encode(str) {
+    let res = "";
+    let place = 0;
+    for (let i = 0; i < str.length; ++i) {
+      while (place >= this.code.length) place -= this.code.length;
+      const shift = this.code[place];
+      const index = this.alph.indexOf(str[i]);
+      place += index !== -1 ? 1 : 0;
+      res += index !== -1 ? this.alph[index + shift] : str[i];
+    }
+    return res;
+  }
+  encrypt(str, key) {
+    if (!str || !key) throw new Error("Incorrect arguments!");
+    this.gencode(key.toUpperCase());
+
+    if (this.direct) return this.encode(str.toUpperCase());
+    return this.encode(str.toUpperCase()).split("").reverse().join("");
+  }
+
+  decode(str) {
+    let res = "";
+    const len = this.abc.length;
+    let place = 0;
+    for (let i = 0; i < str.length; ++i) {
+      while (place >= this.code.length) place -= this.code.length;
+      const shift = this.code[place];
+      const index = this.alph.indexOf(str[i]);
+      place += index !== -1 ? 1 : 0;
+      res += index !== -1 ? this.alph[len + index - shift] : str[i];
+    }
+    return res;
+  }
+  decrypt(str, key) {
+    if (!str || !key) throw new Error("Incorrect arguments!");
+    this.gencode(key.toUpperCase());
+
+    if (this.direct) return this.decode(str.toUpperCase());
+    return this.decode(str.toUpperCase()).split("").reverse().join("");
   }
 }
+/* 
+const machine = new VigenereCipheringMachine();
+console.log(machine.encrypt("attack at dawn!", "alphonse"));
+console.log("AEIHQX SX DLLU!\n");
 
+console.log(machine.decrypt("AEIHQX SX DLLU!", "alphonse"));
+console.log("ATTACK AT DAWN!\n");
+
+const reverseMachine = new VigenereCipheringMachine(false);
+console.log(reverseMachine.encrypt("attack at dawn!", "alphonse"));
+console.log("!ULLD XS XQHIEA\n");
+
+console.log(reverseMachine.decrypt("AEIHQX SX DLLU!", "alphonse"));
+console.log("!NWAD TA KCATTA\n");
+ */
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,26 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) return "Unable to determine the time of year!";
+  if (!(date instanceof Date) || date[Symbol.toStringTag] === "Date")
+    throw new Error("Invalid date!");
+  const month = date.getMonth();
+  if (month >= 11) return "winter";
+  if (month >= 8) return "autumn";
+  if (month >= 5) return "summer";
+  if (month >= 2) return "spring";
+  return "winter";
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
